4f55e5a2f15926acc095ad8bd721eefbc07daa4a
Animesh Singh <animesh.singh@iongroup.com>
2023-11-17 01:24:26 +0530

    Adding assignments

---
+++ Functions/assignment1.py
@@ -0,0 +1,4 @@
+# Build a calculator which supports the following operations: add, subtract, multiply,
+# divide, floor divide, modulus, power
+# The calculator should run indefinitely (hint: use infinite loop) until the user enters a special input.
+# The choice of special input is up to you.
---
+++ Functions/assignment2.py
@@ -0,0 +1,6 @@
+# Ask the user for a list input. The list could contain any number and/or type of elements. Once the user hits enter
+# after entering all the elements of the list, let the user search for an element in the same list. If the element
+# exists, return its position otherwise return a message saying the element doesn't exist
+
+
+# Once this is done, also allow the user to delete an element of the list.
